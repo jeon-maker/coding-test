932d414bd0189707f8b3e12b838cb2dda4faa275
jeon-maker <77326600+jeon-maker@users.noreply.github.com>
2022-06-23 15:31:03 +0900

    Add files via upload

--- 07-02.py
+++ 07-02.py
@@ -11,12 +11,14 @@
 출력예시
 no yes yes
 '''
+import time
 
 n = int(input())
 total = list(map(int,input().split()))
 m = int(input())
 find = list(map(int,input().split()))
 result=[]
+start = time.time()
 for i in range(m):
     result.append("no")
 
@@ -29,3 +31,24 @@ for i in range(m):
 
 for i in range(m):
     print(result[i],end=" ")
+
+print(" \n")
+
+# 이진탐색으로 풀어보기 외우기!
+def binary_research(array,target,start,end):
+    while(start<=end):
+        mid = (start + end) //2
+        if(array[mid]==target):
+            return print("yes",end=" ")
+        elif(array[mid]>target):
+            end = mid-1
+        else:
+            start = mid+1
+    return print("no",end=" ")
+
+total.sort() #total 오름차순 정렬
+for i in range(m):
+    binary_research(total,find[i],0,n-1)
+
+
+print(" \n time : ",time.time()-start)
