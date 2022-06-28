f8050630226769b626ab19019ac3de5b4e4b6039
jeon-maker <77326600+jeon-maker@users.noreply.github.com>
2022-06-29 03:00:16 +0900

    Add files via upload

--- 11-03.py
+++ 11-03.py
@@ -7,20 +7,25 @@
 #부분을 뒤집을 수 있고, 전체를 뒤집을 수 있음.
 # 연속된 묶음을 파악해야함. 즉 연속적인 숫자는 의미가 없음.
 # 축소된 모형이 01010 이면 1을 바꾸는거 두번하면 됨.
-num = list(str(input().split()))
+num = str(input())
+num_arr = []
+for i in range(len(num)):
+    num_arr.append(num[i])
 
 def make_mini(arr):
     mini = []
     for i in range(len(num)-1):
         if(arr[i]!=arr[i+1]):
             mini.append(arr[i]) #전에꺼만 넣자?!
-    mini.append(len(arr)-1) #마지막 것도 넣어서 빠지는 값 없게 해주자
+    mini.append(arr[-1]) #마지막 것도 넣어서 빠지는 값 없게 해주자
     return mini    
         
 def count0or1(arr):
-    cnt0 = arr.count(0)
-    cnt1 = arr.count(1)
+    cnt0 = arr.count('0')
+    cnt1 = arr.count('1')
+    print(cnt0)
+    print(cnt1)
     return(min(cnt0,cnt1))
-print(make_mini(num))
-print(count0or1(make_mini(num)))
+print(make_mini(num_arr))
+print(count0or1(make_mini(num_arr)))
 
