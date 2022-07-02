09250c7970592e4e032ff7a4e5f7b09d8d712382
jeon-maker <77326600+jeon-maker@users.noreply.github.com>
2022-07-02 19:40:37 +0900

    Add files via upload

--- 11-06.py
+++ 11-06.py
@@ -1,22 +1,33 @@
 food_times = list(map(int,input().split()))
 k = int(input())
 num = 0
+
+def find(arr,p):
+    length = len(arr)
+    if(arr[p]==0):
+        while(arr[o]==0):
+            for i in range(length):
+                o = i
+            if(arr[o]!=0):
+                break
+    return o
+
+
+
 def solution(arr,k):
     global num
     length = len(arr)
     for i in range(k):
-        j = i % length
+        j = i % length # j는 현재 0 1 2 - 0 1 2 - 순으로 돌아감.
         if(arr[j]==0):
-            j = j+1
+            j = find(arr,j)
             arr[j] = arr[j]-1
         else:
             arr[j] = arr[j]-1
-        if(j==0):
-            num = 2
-        elif(j==1):
-            num = 3
-        elif(j==2):
-            num = 1
+        num = j+2
+        if(num > length):
+            num = num % length
+
         
         print("j : ",j, " food_times : ",arr)
     return(num)
