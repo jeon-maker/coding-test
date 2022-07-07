20251fa730cb9d048fbeaa1aa05a7dac3059fa1c
jeon-maker <77326600+jeon-maker@users.noreply.github.com>
2022-07-08 01:13:12 +0900

    Add files via upload

--- 11-05.py
+++ 11-05.py
@@ -12,19 +12,45 @@
 25
 
 '''
+from collections import deque
 
-n,m = map(int,input().split())
-ball = list(map(int,input().split()))
+n,m,k,x = map(int,input().split())
+
+visited = [False] * (n+1)
+
+graph = [[]]
+minimum_cost= [[]]
+
+for i in range(n):
+    graph.append([])
+    minimum_cost.append([])
+for i in range(m):
+    s,t = map(int,input().split())
+    graph[s].append(t)
+
+# print(graph)
+#
 cnt = 0
-def choice(arr):
+def bfs(graph,start,visited,minimum_cost,val):
     global cnt
-    L = len(arr)
-    for i in range(L):
-        for j in range(L):
-            if ((i <= j) and (arr[i] != arr[j])):
-                cnt +=1
-            else:
-                pass
+    visited[start]=True
+    queue = deque([start])
+    while queue:
+        v = queue.popleft()
+        cnt +=1
+        # print(v,end=" ")
+        for i in graph[v]:
+            # print(v, " all i is ",i)
+            if not visited[i]:
+                # print( "now i : ",i , " now cnt : ",cnt)
+                queue.append(i)
+                minimum_cost[cnt].append(i)
+                visited[i]=True
+    if minimum_cost[val] ==[]:
+        minimum_cost[val] = -1
 
-choice(ball)
-print(cnt)
+    for i in minimum_cost[val]:
+        print(i)               
+#cnt가 최단거리로 파악가능함.
+
+bfs(graph,1,visited,minimum_cost,k)
