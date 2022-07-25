5f1bc149ca3e9d00759484270568840e1cb6416f
jeon-maker <77326600+jeon-maker@users.noreply.github.com>
2022-07-25 15:03:12 +0900

    Add files via upload

---
+++ boj1449.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n , l = map(int,input().split())
+
+s = list(map(int,input().split()))
+s.sort()
+start = s[0]
+end = s[0]+l
+cnt = 1
+for i in range(n):
+    if start <= s[i] < end:
+        continue
+    else :
+        start = s[i]
+        end = s[i] + l
+        cnt += 1
+print(cnt)
